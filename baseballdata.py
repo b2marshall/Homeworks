f385e6ba2f265d265daf0837eaecc34c5fbfcd82
Marshall Brandenburg <brandenm@prospero.cs.unm.edu>
2021-09-28 16:16:27 -0600

    correctly sorts data into nested lists of speed/frequency pairs

--- baseballdata.py
+++ baseballdata.py
@@ -1,46 +1,64 @@
 import csv 
 
 
-#imports data and seperates data columns into two lists, "untreated_first_column" and "second_column"
 delim = ";"
+#all of this code takes in the data, splits into two columns, sorts, and makes two sorted lists
+#corresponding to column 1 and 2. Each element in these columns is a list with first element speed 
+#and its second element is the corresponding frequency of that speed. 
 
 with open('baseball_data') as fptr:
     lines = fptr.readlines()[2:]
     column_pairs = []
     for element in lines:
         column_pairs.append(element.strip('\n'))
-    
-    second_column = []
-    untreated_first_column = []
-    for element in column_pairs:
-        pair = element.split(',')
-        untreated_first_column.append(pair[0])
-        second_column.append(pair[1])
+untreated_first_column = []
+untreated_second_column = []
+second_column = []
 
+for element in column_pairs:
+    tempsplit = element.split(',')
+    untreated_first_column.append(tempsplit[0])
+    untreated_second_column.append(tempsplit[1])
    
-#sorts both columns and removes empty strings from first_column. 
 untreated_first_column.sort()
-second_column.sort()
+untreated_second_column.sort()
+first_with_counts = []
+second_with_counts = []
+
 first_column = untreated_first_column[untreated_first_column.count(''):]    
+
+for element in first_column:
+    first_with_counts.append([element, untreated_first_column.count(element)])
+for element in untreated_second_column:
+    second_with_counts.append([element, untreated_second_column.count(element)]
+)
+
 
 
 first_no_duplicates = []
 second_no_duplicates = [] 
 count_variable = 0
     
-frequency_pair = [first_column[count_variable], first_column.count(first_column[count_variable])]
-for i in range(0,len(first_column)):
-    if frequency_pair[1]-1 == 0 and count_variable <= len(first_column):
-        first_no_duplicates.append(frequency_pair)
-        count_variable += 1
-        frequency_pair = [first_column[count_variable], first_column.count(first_column[count_variable])] 
-    if frequency_pair[1]-1 !=0 and count_variable <= len(first_column):
-        first_no_duplicates.append(frequency_pair)
-        count_variable += frequency_pair[1]
-        frequency_pair = [first_column[count_variable], first_column.count(first_column[count_variable])] 
+
+for i in range(0,len(first_with_counts)-1):
+    if i == 0:
+        first_no_duplicates.append(first_with_counts[0])
+    if i > 0 and first_no_duplicates[-1] != first_with_counts[i]:
+        first_no_duplicates.append(first_with_counts[i])
+    else:
+        pass
+
+for i in range(0,len(second_with_counts)-1):
+    if i == 0:
+        second_no_duplicates.append(second_with_counts[0])
+    if i > 0 and second_no_duplicates[-1] != second_with_counts[i]:
+        second_no_duplicates.append(second_with_counts[i])
+    else:
+        pass  
 
 
-        
+print(first_no_duplicates)
+print('*'*80)
+print(second_no_duplicates)
 
-print(first_no_duplicates) 
-
+#the following code takes these two lists of lists and generates the graph. 
