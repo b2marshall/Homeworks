117fd7b8a0bca535efe25b9943b386d55a926889
b2marshall <b2marshall@yahoo.com>
2021-09-30 21:23:05 -0600

    generates better plot with labels and adds option for default run

--- project1.py
+++ project1.py
@@ -9,30 +9,36 @@ from random import choice
 
 
 #handles user input and provides simple guardrails for values of the parameters
-m = int(input("m value?\n"))
-if m <= 0: 
-    print("invalid value! m value must be positive. \n")
-
+if int(input("use default? Input '0' for no, and 1 for yes.\n")) == 0: 
     m = int(input("m value?\n"))
+    if m <= 0: 
+        print("invalid value! m value must be positive. \n")
 
-a = int(input("a value?\n"))
-if a <= 0 or a >= m:
-    print("invalid value for a. a must be positive and less than m!\n")
+        m = int(input("m value?\n"))
+
     a = int(input("a value?\n"))
+    if a <= 0 or a >= m:
+       print("invalid value for a. a must be positive and less than m!\n")
+       a = int(input("a value?\n"))
 
-c = int(input("c value?\n"))
-if c < 0 or c > m:
-    print("invalid value for c. c must be nonnegative and less than m!\n")
-    c = int(input("c value?\n")) 
-x_0 = int(input ("x_0 value?\n"))
-if x_0 < 0 or x_0 > m: 
-    print("invalid value for x_0. x_0 must be nonnegative and less than m!\n")
-    int(input("x_0 value?\n")) 
-N = int(input ("N value?\n"))
-if N <= 0: 
-    print("invalid N! N must be a positive integer.\n")
-    N = int(input("N value?\n")) 
-
+    c = int(input("c value?\n"))
+    if c < 0 or c > m:
+        print("invalid value for c. c must be nonnegative and less than m!\n")
+        c = int(input("c value?\n")) 
+    x_0 = int(input ("x_0 value?\n"))
+    if x_0 < 0 or x_0 > m: 
+        print("invalid value for x_0. x_0 must be nonnegative and less than m!\n")
+        int(input("x_0 value?\n")) 
+    N = int(input ("N value?\n"))
+    if N <= 0: 
+        print("invalid N! N must be a positive integer.\n")
+        N = int(input("N value?\n")) 
+else:
+    m = 4294967269
+    a = 3
+    c = 7 
+    x_0 = 0
+    N = 1000000
 #generates the sequence of random numbers from LCG 
 random_sequence = [x_0] 
 for i in range(0, N-1):
@@ -56,8 +62,22 @@ def mean_function(list_nums):
     tempvar /= len(list_nums)
     return tempvar
 
+
 number_bins = 100000
+plot_bound_xs = max(random_sequence)  
 
 fig, axs = plt.subplots(1,1, sharey=True, tight_layout=True)
 axs.hist(random_sequence, bins=number_bins)
+plt.ylim(0,10)
+#plt.yticks([0,1,2,3,4,5])
+#plt.xlim(0, plot_bound_xs)
+#plt.figure(figsize=(7,5))
+plt.title("Frequency of random number values")
+plt.ylabel("Frequency of particular value")
+plt.xlabel("Value from our randomly generated sequence")
 plt.savefig("histogram.png")
+
+#for a test run, m = 4294967296 = 2^32 > 100,000 and a = 3, c = 7 , x_0 = 0 and 
+# N = 100,000 makes a good plot.
+
+ 
