9414bea156bbb82706c6aec21e43ae967f380b5b
b2marshall <b2marshall@yahoo.com>
2021-10-07 17:49:41 -0600

    now plots with confidence bands, legends and labels

--- hw/baseballdata.py
+++ hw/baseballdata.py
@@ -3,7 +3,7 @@ import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt  
-
+import math
 
 
 delim = ";"
@@ -26,7 +26,7 @@ for element in column_pairs:
     untreated_second_column.append(tempsplit[1])
    
 untreated_first_column.sort()
-untreated_second_column.sort()
+second_column = untreated_second_column.sort()
 
 first_column = untreated_first_column[untreated_first_column.count(''):]    
 
@@ -36,29 +36,55 @@ for element in first_column:
     if element not in first_singles: 
         first_singles.append(element)
 
-def ecdfys(dat_column_singles,dat_column,n):
+
+def ecdf(xs, data): 
     ys = [0]
-    for element in dat_column_singles: 
-        ys.append(ys[-1]+dat_column.count(element)/n)
+    temps = 0
+    for element in xs:
+        for x in data:
+            if float(x) <= float(element): 
+                temps+=1
+        ys.append(temps/len(data))
+              
+        temps =0
     return ys[1:]
 
+print(np.linspace(80,92,num=100))
+print(min(first_column), max(first_column))
 #the following code takes these two lists of lists and generates the graph.
-xs = [0]
-for i in range(0,len(first_column)):
-    if xs[-1] != float(first_column[i]):
-        xs.append(float(first_column[i]))
-xs1 = xs[1:]
+mu = np.mean([float(first_column[i]) for i in range(0,len(first_column))])
+sigma = math.sqrt(np.var([float(first_column[i]) for i in range(0,len(first_column))]))
+x = ecdf(np.random.normal(mu,sigma, size=100), first_column)
 
 
-dat_array = np.array(first_column)
-n1 = len(first_column) 
-#y1 = np.arange(start=1/n1, stop=(n1+1)/n1, step=1/n1) 
-plt.plot(X,CY)
+alpha = 0.05
+def epsilon_n(alpha,n):
+    output = math.sqrt((1/(2*n))*np.log(2/alpha))
+    return output 
+
+def upper(n,alpha,xs,data):
+    epsilon = epsilon_n(alpha,n)
+    upperbound = [max((ecdf(xs,data)[i]+epsilon),0) for i in range(0,len(xs))]
+    return upperbound 
+
+def lower(n,alpha,xs,data):
+    epsilon = epsilon_n(alpha,n) 
+    lowerbound = [min(ecdf(xs,data)[i]-epsilon,1) for i in range(0,len(xs))]
+    return lowerbound
+
+#upper_bound1 = upper(len(first_column), alpha,)
 
 
-#plt.plot(dat_array, y1)
-plt.figure(figsize=(27,13))
-plt.step(first_singles,ecdfys(first_singles,first_column,len(first_column)), 'r*', where='post')
+plt.figure(figsize=(12,9))
+plt.title("ECDF for 2015 baseball data")
+plt.yticks(np.linspace(0,1.01,num=30))
+plt.ylabel("P(X_i) >= x")
+plt.plot(np.linspace(81.5,91,num=100),upper(len(first_column),alpha,np.linspace(81.5,91,num=100),first_column), label='upper confidence bound, alpha=0.05', color='black')
+#plt.hist(x, bins= 200, normed=True, cumulative=True, label='CDF', histtype='step', alpha=0.55, color='k')
+plt.xlabel("Pitch speeds in mph")
+plt.plot(np.linspace(81.5,91,num=100),lower(len(first_column),alpha,np.linspace(81.5,91,num=100),first_column), label='lower confidence bound, alpha = 0.05', color='blue')
+#plt.step(first_singles,ecdfys(first_singles,first_column,len(first_column)), 'r*', where='post')
+plt.step(np.linspace(81.5,91,num=100), ecdf(np.linspace(81.5,91,num=100),first_column), 'r*', where='post', label='My ECDF')
+plt.legend(loc='upper left')
 plt.savefig("npECDF1.png")
-print(first_singles)
-print(ecdfys(first_singles,first_column,len(first_column)))
+
